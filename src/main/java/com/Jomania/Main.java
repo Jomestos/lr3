9179de8d29258c0261a020b0d0ec5b78edfd94d2
Dmytro <d.ponomarev267@gmail.com>
2025-11-01 12:24:48 +0200

    test commit 2

--- src/main/java/com/Jomania/Main.java
+++ src/main/java/com/Jomania/Main.java
@@ -3,6 +3,6 @@ package com.Jomania;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("test commit");
+        System.out.println("test commit #2");
     }
 }
